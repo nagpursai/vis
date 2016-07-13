a6f64122c5d341fec7849cc3b810ca542a539b32
Ajay Kumar <Ajay.Kumar@synchronoss.com>
2016-07-13 08:42:26 +0530

    added findAll method and connect with db

--- vis-rest/src/main/java/com/vis/rest/user/controller/UserController.java
+++ vis-rest/src/main/java/com/vis/rest/user/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,36 +15,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vis.rest.user.model.User;
+import com.vis.rest.user.repository.UserRepository;
 
 @RestController
 @RequestMapping("/user")
 
 public class UserController {
-	/*@RequestMapping("/test")
-	public String test(){
-		System.out.println("Hi Test");
-		return "Hello Vis";
-	}*/
+	@Autowired
+	UserRepository userRepository;
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public User registerUser(@RequestBody User user){
 		//To save data in to database.
 		System.out.println(user.getFirstName()+"  "+user.getEmail());
 		return user;
 	}
-	@RequestMapping(value = "/getUser/{firstName}", method = RequestMethod.GET)
-	public ResponseEntity<User> getUser(@PathVariable String firstName){
+	@RequestMapping(value = "/getUser/{email}", method = RequestMethod.GET)
+	public ResponseEntity<User> getUser(@PathVariable String email){
 		//To save data in to database.
+		/*System.out.println("FirstName:"+firstName);
 		User user = new User();
 		user.setEmail("toajay.info@gmail.com");
 		user.setFirstName("Ajay");
-		user.setLastName("KUmar");
+		user.setLastName("KUmar");*/
+		User user = userRepository.findByEmail(email);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-	@RequestMapping(value = "/getAllUser/{firstName}", method = RequestMethod.GET)
-	public ResponseEntity<List<User>> getAllUser(@PathVariable String firstName){
+	@RequestMapping(value = "/getAllUser", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> getAllUser(){
 		//To save data in to database.
-		System.out.println("First Name="+firstName);
-		List<User> userList = new ArrayList<User>();
+		/*List<User> userList = new ArrayList<User>();
 		User user = new User();
 		user.setEmail("toajay.info@gmail.com");
 		user.setFirstName("Ajay");
@@ -53,7 +53,8 @@ public class UserController {
 		user1.setEmail("ajay.info@gmail.com");
 		user1.setFirstName("Ajay");
 		user1.setLastName("KUmar");
-		userList.add(user1);
+		userList.add(user1);*/
+		List<User> userList = userRepository.findAll();
 		return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
 	}
 }
--- vis-rest/src/main/java/com/vis/rest/user/repository/UserRepository.java
+++ vis-rest/src/main/java/com/vis/rest/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.vis.rest.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -8,4 +10,6 @@ import com.vis.rest.user.model.User;
 public interface UserRepository extends CrudRepository<User, Long>{
 	 User findByEmail(String email);
 	 User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+	 List<User> findAll();
+	 //User findOne(long id);
 }
