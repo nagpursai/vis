4d2025397149a9cb5461a11e08681039c48ea7ad
nikki <java.sonaligupta@gmail.com>
2017-03-05 15:29:11 +0530

    Added name

--- vis-rest/src/main/java/com/vis/rest/SwaggerConfig.java
+++ vis-rest/src/main/java/com/vis/rest/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//I'm Sonali
 @Configuration
 @EnableSwagger2
 @EnableWebMvc
